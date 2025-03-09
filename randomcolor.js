1d371225ff83eab897e5a921c0ad074b286a82f7
rakibhossain2025 <rakibhossain4230@gmail.com>
2025-03-09 19:54:21 +0600

    sad

--- randomcolor.js
+++ randomcolor.js
@@ -1,31 +1,32 @@
-// function ramdom(){
-//   const r = parseInt(200 + Math.random() * 56);
-//   const g = parseInt(200 + Math.random() * 56);
-//   const b = parseInt(200 + Math.random() * 56);
-//   console.log(`${b},${g}, ${r}`)
-//   return `${b},${g}, ${r}`
-// }
-//   document.getElementById("theme-change").addEventListener("click", function (){
-//     document.body.style.backgroundColor = ramdom()
-//   })
-// document.getElementById("theme-change").addEventListener("click", function () {
-//   function ramdom() {
-//     const r = parseInt(200 + Math.random() * 56);
-//     const g = parseInt(200 + Math.random() * 56);
-//     const b = parseInt(200 + Math.random() * 56);
-//     return `${b},${g}, ${r}`;
-//   }
-//   document.body.style.backgroundColor= ramdom()
-// });
+function ramdom(){
+  const r = parseInt(200 + Math.random() * 56);
+  const g = parseInt(200 + Math.random() * 56);
+  const b = parseInt(200 + Math.random() * 56);
+  console.log(`${b},${g}, ${r}`)
+  return `rgb(${b},${g}, ${r})`
+}
+  document.getElementById("theme-change").addEventListener("click", function (){
+    document.body.style.backgroundColor = ramdom()
+  })
+document.getElementById("theme-change").addEventListener("click", function () {
+  function ramdom() {
+    const r = parseInt(200 + Math.random() * 56);
+    const g = parseInt(200 + Math.random() * 56);
+    const b = parseInt(200 + Math.random() * 56);
+    return `rgb(${b},${g}, ${r})`;
+  }
+  document.body.style.backgroundColor= ramdom()
+});
 document.getElementById("theme-change").addEventListener("click", function () {
   function getRandomLightColor() {
     const r = Math.floor(200 + Math.random() * 56);
     const g = Math.floor(200 + Math.random() * 56);
     const b = Math.floor(200 + Math.random() * 56);
+    console.log(r,b,g)
     return `rgb(${r}, ${g}, ${b})`;
   }
 
   document.body.style.backgroundColor = getRandomLightColor();
-});
-
-
+;
+getRandomLightColor()
+})
